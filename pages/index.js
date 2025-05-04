ddb6354f8c230abef0e940cfb8e418539f852bb2
Hiro <feel.so.happy7+git@gmail.com>
2025-05-05 00:13:10 +0900

    Remove unused useEffect import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { supabase } from '../lib/supabaseClient'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 export default function Home() {
   const [freeTerm, setFreeTerm] = useState('')
